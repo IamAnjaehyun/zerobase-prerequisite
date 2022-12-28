dcd6471840103ced149bef4ea5d770c120f66ead
Anjaehyun <lvet0330@gmail.com>
2022-12-28 15:45:11 +0900

    # 인터페이스
    게임 캐릭터와 NPC 만들어보기를 통해 인터페이스 학습
    다중상속처럼 사용 가능 / 추상메소드와 상수만으로 이루어짐

--- 컴퓨터적 사고를 위한 Java 기초/java/src/Java_11/src/Main.java
+++ 컴퓨터적 사고를 위한 Java 기초/java/src/Java_11/src/Main.java
@@ -1,14 +1,19 @@
 package Java_11.src;// Java 프로그래밍 - 인터페이스
-
+//다중상속처럼 사용 가능 / 추상메소드와 상수만으로 이루어짐
 // School 인터페이스
 interface School {
-    public static final int MAX_CLASS = 20;
-    public static final int MAX_PERSON_PER_CLASS = 40;
+    public static final int MAX_CLASS = 20; //학급수
+    public static final int MAX_PERSON_PER_CLASS = 40; //학급당학생수
     public abstract void printSchool();
 }
 
 // Student 클래스 - School 인터페이스 이용
-
+class Student implements School{
+    @Override
+    public void printSchool() {
+        System.out.println("OO Univ");
+    }
+}
 
 // Person 클래스
 class Person {
@@ -21,7 +26,16 @@ class Person {
 
 
 // Student2 클래스 - Person 상속, School 인터페이스 이용
+class Student2 extends Person implements School {
 
+    Student2(String name){
+        super.name = name;
+    }
+    @Override
+    public void printSchool() {
+        System.out.println("11 Univ");
+    }
+}
 
 
 public class Main {
@@ -30,17 +44,17 @@ public class Main {
 
 //      1. 인터페이스 기본 사용
         System.out.println("== 기본 인터페이스 ==");
-//        Student s1 = new Student();
-//        s1.printSchool();
-//        System.out.println(s1.MAX_CLASS);
-//        System.out.println(s1.MAX_PERSON_PER_CLASS);
+        Student s1 = new Student();
+        s1.printSchool();
+        System.out.println(s1.MAX_CLASS); //인터페이스쪽 상수
+        System.out.println(s1.MAX_PERSON_PER_CLASS); //인터페이스쪽 상수
 
 
 //      2. 다중 상속처럼 사용하기
         System.out.println("== Like 다중 상속 ==");
-//        Student2 s2 = new Student2("A");
-//        s2.printSchool();
-//        s2.printName();
+        Student2 s2 = new Student2("A");
+        s2.printSchool();
+        s2.printName();
 
     }
 
