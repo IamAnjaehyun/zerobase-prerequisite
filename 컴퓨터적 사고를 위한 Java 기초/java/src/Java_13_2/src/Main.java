5907cee35bb13a558b89ff6f03cdcbf2ab622ae6
Anjaehyun <lvet0330@gmail.com>
2022-12-28 16:24:15 +0900

    # 입출력_2 (파일입출력)
    파일에서 Scanner BufferedReader FileWriter/Reader 사용 txt 내 단어 변경

--- 컴퓨터적 사고를 위한 Java 기초/java/src/Java_13_2/src/Main.java
+++ 컴퓨터적 사고를 위한 Java 기초/java/src/Java_13_2/src/Main.java
@@ -7,17 +7,41 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //      1. 파일 쓰기
 //      FileWriter
-        
+        FileWriter fw = new FileWriter("./java_13_2.txt");
+        String memo = "헤드 라인\n";
+        fw.write(memo);
 
+        memo = "1월 1일 날씨 맑음 \n";
+        fw.write(memo);
+
+        fw.close();
 //      PrintWriter
-        
+        PrintWriter pw = new PrintWriter("./java_13_2.txt");
+        memo = "헤드 라인 - PrintWriter";
+        pw.println(memo);
 
+        memo = "1월 1일 날씨 맑음";
+        pw.println(memo);
+        pw.close();
 //      파일 이어 쓰기
+        FileWriter fw2 = new FileWriter("./java_13_2.txt",true);
+
+        memo = "1월 2일 날씨 완전 맑음\n";
+        fw2.write(memo);
+        fw2.close();
 
 
-
+        PrintWriter pw2 = new PrintWriter(new FileWriter("./java_13_2.txt",true));
+        memo = "1월 3일 날씨 또 맑음";
+        pw2.println(memo);
+        pw2.close();
 //      2. 파일 입력
-
+        BufferedReader br = new BufferedReader(new FileReader("./java_13_2.txt"));
+        while(true){
+            String line = br.readLine();
+            if(line == null){break;}
+            System.out.println(line);
+        }
 
     }
 }
