ba2128e39534bd999322a52cbb0de81c76a09de7
Anjaehyun <lvet0330@gmail.com>
2022-12-30 16:24:37 +0900

    # 연습문제 풀이 2_1
    1. 로마 숫자 표기를 정수형으로 변환하는 프로그램을 작성하세요.
    2. 정수형 숫자를 로마 숫자 표기로 변환하는 프로그램을 작성하세요.
    3. 간단한 편집기를 구현하려고 한다.

--- 컴퓨터적 사고를 위한 Java 기초/java/src/Java_18_2/src/Practice3.java
+++ 컴퓨터적 사고를 위한 Java 기초/java/src/Java_18_2/src/Practice3.java
@@ -1,8 +1,39 @@
 package Java_18_2.src;
 
+import java.util.ArrayList;
+
 public class Practice3 {
     public static String solution(String input, String cmd) {
-        return null;
+        StringBuffer sb = new StringBuffer(input); //문자열이 자주 바뀔때 유용함
+        ArrayList<String> cmdArr = new ArrayList<>();
+
+        for (String s : cmd.split(" ")) {
+            cmdArr.add(s);
+        }
+
+        int curSor = sb.length(); //맨끝이 커서위치
+        int cmdIdx = 0;
+        while (cmdIdx != cmdArr.size()) { //가장 끝에있는 커맨드 실행할때까지
+            String cur = cmdArr.get(cmdIdx); //명령어 하나씩 꺼내기
+            if (cur.equals("L")) {
+                curSor = Math.max(0, curSor - 1); //음수로 가면 안되니 적어도 0은 보장
+            } else if (cur.equals("D")) {
+                curSor = Math.min(sb.length(), curSor + 1); //문자열 넘어가면 안되니 끝거리는 len
+            } else if (cur.equals("B")) {
+                if (curSor == 0) {
+                    cmdIdx++;
+                    continue;
+                }
+                sb.delete(curSor - 1, curSor); //좌측 한 데이터 지우기
+                curSor = Math.max(0, curSor -1);
+            } else if (cur.equals("P")) {
+                String s = cmdArr.get(++cmdIdx);
+                sb.insert(curSor, s); //커서에 꺼낸거 위치해줌
+                curSor += 1;
+            }
+            cmdIdx++;
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
