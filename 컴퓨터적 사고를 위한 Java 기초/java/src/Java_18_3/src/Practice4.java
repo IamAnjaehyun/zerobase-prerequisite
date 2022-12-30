3adb1eb7d5254cda4792954dc93589875c103784
Anjaehyun <lvet0330@gmail.com>
2022-12-30 18:26:20 +0900

    # 연습문제 풀이 3_2
    4. 2차원 정수형 배열 matrix 에 주어지면 나선형 모양으로 출력하는 프로그램을 작성하세요
    5. 높이 값들이 주어졌을 때, 벽 사이사이의 빈 공간에 담을 수 있는 물의 총량을 출력하세요. *복습필요*

--- 컴퓨터적 사고를 위한 Java 기초/java/src/Java_18_3/src/Practice4.java
+++ 컴퓨터적 사고를 위한 Java 기초/java/src/Java_18_3/src/Practice4.java
@@ -2,18 +2,55 @@ package Java_18_3.src;
 
 import java.util.ArrayList;
 
+//빙글빙글 출력
 public class Practice4 {
     public static ArrayList<Integer> solution(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return null;
+        }
+        ArrayList<Integer> result = new ArrayList<>();
 
-        return null;
+        int rowStart = 0;
+        int rowEnd = matrix.length - 1;
+        int colStart = 0;
+        int colEnd = matrix[0].length - 1; //정사각형 아닐 수 있으니
+
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            //오른쪽방향
+            for (int i = colStart; i <= colEnd; i++) {
+                result.add(matrix[rowStart][i]);
+            }
+            rowStart++;
+            //오른쪽끝에서 아래로
+            for (int i = rowStart; i <= rowEnd; i++) {
+                result.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+            //왼쪽갈때
+            if (rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    result.add(matrix[rowEnd][i]);
+                }
+            }
+            rowEnd--; //출력됐으면 한칸 올라감
+
+            //위로올라갈때
+            if(colStart <=colEnd){
+                for(int i=rowEnd; i>= rowStart; i--){
+                    result.add(matrix[i][colStart]);
+                }
+            }
+            colStart++;
+        }
+        return result;
     }
-    
+
     public static void main(String[] args) {
         // Test code
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         System.out.println((solution(matrix)));
 
-        matrix = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10 ,11 ,12}};
+        matrix = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
         System.out.println((solution(matrix)));
     }
 }
--- 컴퓨터적 사고를 위한 Java 기초/java/src/Java_18_3/src/Practice5.java
+++ 컴퓨터적 사고를 위한 Java 기초/java/src/Java_18_3/src/Practice5.java
@@ -2,7 +2,34 @@ package Java_18_3.src;
 
 public class Practice5 {
     public static int solution(int[] height) {
-        return 0;
+        if(height==null || height.length ==0){
+            return 0;
+        }
+
+        int left = 0;
+        int right = height.length-1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int result = 0;
+
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= leftMax){
+                    leftMax = height[left];
+                }else{
+                    result += leftMax - height[left]; //왼쪽
+                }
+                left++;
+            }else{
+                if(height[right] >= rightMax){
+                    rightMax = height[right];
+                }else{
+                    result += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return result;
     }
     
     public static void main(String[] args) {
