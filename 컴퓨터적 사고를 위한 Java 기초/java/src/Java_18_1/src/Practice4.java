eaecb4aa5ddcb39be2c0933b6bfb89d748167b74
Anjaehyun <lvet0330@gmail.com>
2022-12-30 15:45:05 +0900

    # 연습문제 풀이 1_2
    4. 5 + 1 가지 별찍기
    5. 어떤 두 벽을 고르면 가장 많은 물을 담을 수 있는지를 확인하고 그 때의 면적을 출력하기

--- 컴퓨터적 사고를 위한 Java 기초/java/src/Java_18_1/src/Practice4.java
+++ 컴퓨터적 사고를 위한 Java 기초/java/src/Java_18_1/src/Practice4.java
@@ -2,39 +2,111 @@ package Java_18_1.src;
 
 public class Practice4 {
     public static void solution(int n, int type) {
-
+        if(type == 1){
+            type1(n);
+        }else if(type == 2){
+            type2(n);
+        }else if(type == 3){
+            type3(n);
+        }else if(type == 4){
+            type4(n);
+        }else if(type == 5) {
+            type5(n);
+        }else if(type == 6) {
+            type6(n);
+        }
     }
 
     public static void type1(int n) {
         System.out.println("== Type1 ==");
-
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         System.out.println();
     }
 
     public static void type2(int n) {
         System.out.println("== Type2 ==");
-
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         System.out.println();
     }
 
     public static void type3(int n) {
         System.out.println("== Type3 ==");
-
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j < n - i - 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
         System.out.println();
     }
 
     public static void type4(int n) {
         System.out.println("== Type4 ==");
-
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i * 2 + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         System.out.println();
     }
 
     public static void type5(int n) {
         System.out.println("== Type5 ==");
-
+        //상단부
+        for (int i = 0; i <= n / 2; i++) {
+            for (int j = 0; j < n / 2 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i * 2 + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //하단부
+        for (int i = n / 2; i > 0; i--) {
+            for (int j = 0; j < n / 2 + 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         System.out.println();
     }
 
+    public static void type6(int n){
+        for(int i=0;i<n;i++){
+            //공백
+            for(int j=0;j<n/2 -Math.abs(i-(n/2)); j++){
+                System.out.print(" ");
+            }
+            //별
+            for(int k=0;k<n-(n/2 - Math.abs(n/2-i))*2;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
     public static void main(String[] args) {
         // Test code
         solution(3, 1);
@@ -42,5 +114,6 @@ public class Practice4 {
         solution(3, 3);
         solution(3, 4);
         solution(7, 5);
+        solution(9, 6); //모래시계 내가 추가
     }
 }
--- 컴퓨터적 사고를 위한 Java 기초/java/src/Java_18_1/src/Practice5.java
+++ 컴퓨터적 사고를 위한 Java 기초/java/src/Java_18_1/src/Practice5.java
@@ -2,7 +2,26 @@ package Java_18_1.src;
 
 public class Practice5 {
     public static int solution(int[] height) {
-        return 0;
+        int left = 0;
+        int right = height.length -1;
+        int maxArea = 0;
+
+        while(left < right){ //오른쪽벽이 왼쪽벽 침범하면 중단
+            int x = right - left;
+            int y = height[left] < height[right] ? height[left] : height[right]; //높이 더 낮은쪽으로 height 넘치면안됨
+//            int curArea = x*y; //계산한 면적
+//            maxArea = maxArea > curArea ? maxArea : curArea;
+
+            int curArea = x * Math.min(height[left], height[right]); //계산한 면적
+            maxArea = Math.max(maxArea,curArea); //더 큰거나오면 갱신
+
+            if(height[left] < height[right]){ //둘 중에 더 큰벽을 남기고 작은벽을 한칸씩 이동
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return maxArea;
     }
 
     public static void main(String[] args) {
