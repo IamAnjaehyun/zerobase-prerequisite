03e6cbb30deaf6d4e9d5d307e45948513ed8ba8b
Anjaehyun <lvet0330@gmail.com>
2023-01-02 12:46:04 +0900

    # 기초수학 - 조합
    // 조합 nCr = n! / (n-r)!r!
           // 서로다른 n개 중에서 r개를 선택하는 경우의 수(순서X 중복X) ex)서로다른 4명 중 주번 2명 뽑는 방법
    
     // 중복 조합 nHr = n+r - 1Cr 
            // 서로다른 n개 중에서 r개를 선택하는 경우의 수(순서X 중복O) ex)후보 2명 유권자 3명일 때 무기명 투표 방법

---
+++ 자료구조 및 알고리즘/Chapter01_Math/src/java05combination/Practice.java
@@ -0,0 +1,39 @@
+package java05combination;
+
+// Practice
+// 1,2,3,4 를 이용하여 세자리 자연수 만드는 방법 (순서x 중복x) 의 각 결과를 출력하시오
+
+public class Practice {
+    void combination(int[] arr, boolean[] visited, int depth, int n, int r) {
+
+        if (r == 0) {
+            for (int i = 0; i < n; i++) {
+                if (visited[i]) {
+                    System.out.print(arr[i] + " ");
+                }
+            }
+            System.out.println();
+            return;
+        }
+
+        if (depth == n) {
+            return;
+        }
+
+        visited[depth] = true;
+        combination(arr, visited, depth + 1, n, r - 1);
+
+        visited[depth] = false;
+        combination(arr, visited, depth + 1, n, r);
+
+    }
+
+    public static void main(String[] args) {
+        // TestCode
+        int[] arr = {1, 2, 3, 4};
+        boolean[] visited = {false, false, false, false};
+
+        Practice p = new Practice();
+        p.combination(arr, visited, 0, 4, 3);
+    }
+}
