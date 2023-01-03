69717e1cfc822bc70c9e0cfb6e92cde078ec60c6
IamJaehyun <lvet0330@gmail.com>
2023-01-03 17:03:07 +0900

    # 선형자료구조 - 배열 문제 풀이_2
    5.배열 arr의 값을 오름차순으로 출력
    6.배열 arr에서 중복 값을 제거한 새 배열을 만드시오.
    7.2차원 배열 arr을 시계방향 90도 회전시킨 결과를 출력하세요.

---
+++ 자료구조 및 알고리즘/Chapter02_DataStructure/src/java02array/practice/Practice5.java
@@ -0,0 +1,25 @@
+package java02array.practice;
+
+//배열 arr의 값을 오름차순으로 출력
+
+import java.util.Arrays;
+
+//입출력 예시)
+//arr : 5, 3, 1, 4, 6, 1
+//arr : 1, 1, 3, 4, 5, 6
+public class Practice5 {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 1, 4, 6, 1};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
