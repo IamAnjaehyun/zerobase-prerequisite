6087014808fdbabfb3b20faf6f05e994e3b22150
Anjaehyun <lvet0330@gmail.com>
2023-01-21 14:16:16 +0900

    # 알고리즘 - 다이나믹 프로그래밍
    다이나믹 프로그래밍(DP) , 동적 계획법
    - 큰 문제를 부분 문제로 나눈 후 답을 찾아가는 과정에서, 
      계산된 결과를 기록하고 재활용하며 문제의 답을 구하는 방식
    - 중간 계산 결과를 기록하기 위한 메모리가 필요
    - 한 번 계산한 문제를 다시 계산하지 않아 속도가 빠름
    
    Main. 피보나치 수열을 이용한 다이나믹 프로그래밍 풀이

---
+++ 자료구조 및 알고리즘/Chapther04_Algorithm/src/java07dp/Main.java
@@ -0,0 +1,50 @@
+package java07dp;
+
+//알고리즘 - 다이나믹 프로그래밍 ( 피보나치 수열 )
+public class Main {
+    //피보나치 수열 (일반 풀이 - O(n^2)
+    //계산했던 부분도 다시 계산
+    public static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+    //피보나치 수열 (DP 풀이 - 타뷸레이션 - O(n)) 아래서부터
+    public static int fibDP(int n) {
+        int[] dp = new int[n < 2 ? 2 : n + 1]; //저장소
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    //피보나치 수열 (DP 풀이 - 메모이제이션 - O(n)) 위에서부터
+    static int[] dp = new int[8];
+
+    public static int fibDP2(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        dp[n] = fibDP2(n - 1) + fibDP2(n - 2);
+
+        return dp[n];
+     
+    }
+
+    public static void main(String[] args) {
+        //TestCode
+        System.out.println(fib(7));
+        System.out.println(fibDP(7));
+        System.out.println(fibDP2(7));
+    }
+
+}
