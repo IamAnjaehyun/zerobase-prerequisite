7f1f4a40b9e065a3ce0236e734696abc6c888ebb
Anjaehyun <lvet0330@gmail.com>
2023-01-22 16:31:22 +0900

    # 알고리즘 - 최단경로 다익스트라
    최단경로 알고리즘
    - 가중 그래프 상의 두 노드를 연결하는 가장 짧은 경로를 찾는 방법
    - 지도 경로 탐색, 네트워크 구축에 드는 비용을 최소화 하는데 사용
    - 최단 경로 알고리즘
    > 다익스트라(Dijkstra)
      - 출발점에서 목표점까지의 최단 경로를 구하는 알고리즘
      - 한 노드에서 다른 모든 노드로의 최단 경로를 구할 수 있음
      - <b>간선에서 음의 가중치가 없어야 함</b>
      - 그리디 + DP 형태
      - 알고리즘 복잡도 : O(ElogV)  > 간선수log노드수
    
    Dijkstra. 다익스트라
    Dijkstra2. 다익스트라 우선순위 큐 사용

---
+++ 자료구조 및 알고리즘/Chapther04_Algorithm/src/java10shortestpath/Dijkstra.java
@@ -0,0 +1,68 @@
+package java10shortestpath;
+
+import java.util.ArrayList;
+
+public class Dijkstra {
+    static class Node {
+        int to;
+        int weight;
+
+        public Node(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+
+    public static void dijkstra(int v, int[][] data, int start) {
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+        for (int i = 0; i < v + 1; i++) { //노드번호가 1부터 시작을 해서 편의를 위해
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            graph.get(data[i][0]).add(new Node(data[i][1], data[i][2]));
+        }
+
+        int[] dist = new int[v + 1]; //최단거리 경로 기록할 dp용 메모리
+        for (int i = 1; i < v + 1; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+        dist[start] = 0;
+
+        boolean[] visited = new boolean[v + 1];
+
+        for (int i = 0; i < v; i++) {
+            int minDist = Integer.MAX_VALUE;
+            int curIdx = 0;
+            for (int j = 1; j < v + 1; j++) {
+                if (!visited[j] && dist[j] < minDist) { //방문한적 없고 min 보다 작아서 최소
+                    minDist = dist[j];
+                    curIdx = j;
+                }
+            }
+
+            visited[curIdx] = true;
+
+            for (int j = 0; j < graph.get(curIdx).size(); j++) { //인접노드 사이즈
+                Node adjNode = graph.get(curIdx).get(j);
+                if (dist[adjNode.to] > dist[curIdx] + adjNode.weight) { //원래 거리정보 배열에 있던 값 보다 현재 거리랑 + 거쳐서가는 해당 비용이 작으면 업데이트
+                    dist[adjNode.to] = dist[curIdx] + adjNode.weight;
+                }
+            }
+        }
+        for (int i = 1; i < v + 1; i++) {
+            if (dist[i] == Integer.MAX_VALUE){
+                System.out.println("INF ");
+            }else{
+                System.out.print(dist[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        //TestCode
+        int[][] data = {{1, 2, 2}, {1, 3, 3}, {2, 3, 4}, {2, 4, 5}, {3, 4, 6}, {5, 1, 1}};
+        dijkstra(5, data, 1);
+    }
+}
---
+++ 자료구조 및 알고리즘/Chapther04_Algorithm/src/java10shortestpath/Dijkstra2.java
@@ -0,0 +1,74 @@
+package java10shortestpath;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+//다익스트라 우선순위 큐 사용
+public class Dijkstra2 {
+
+    static class Node {
+        int to;
+        int weight;
+
+        public Node(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+
+    public static void dijkstra(int v, int[][] data, int start) {
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+        for (int i = 0; i < v + 1; i++) { //노드번호가 1부터 시작을 해서 편의를 위해
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            graph.get(data[i][0]).add(new Node(data[i][1], data[i][2]));
+        }
+
+        int[] dist = new int[v + 1]; //최단거리 경로 기록할 dp용 메모리
+        for (int i = 1; i < v + 1; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+        dist[start] = 0;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>((x, y) -> x.weight - y.weight); //우선순위 weight 기준 minheap 작은것부터 바로 튀어나옴 최소간선 ㄱㅊ
+        pq.offer(new Node(start, 0));
+
+        while (!pq.isEmpty()) {
+            Node curNode = pq.poll();
+
+            if(dist[curNode.to] < curNode.weight){
+                continue;
+            }
+
+            for (int i = 0; i < graph.get(curNode.to).size(); i++) {
+                Node adjNode = graph.get(curNode.to).get(i);
+
+                if(dist[adjNode.to] > curNode.weight + adjNode.weight){
+                    dist[adjNode.to] = curNode.weight + adjNode.weight;
+                    pq.offer(new Node(adjNode.to, dist[adjNode.to]));
+                }
+
+            }
+        }
+
+
+        //출력
+        for (int i = 1; i < v + 1; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
+                System.out.println("INF ");
+            } else {
+                System.out.print(dist[i] + " ");
+            }
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        //TestCode
+        int[][] data = {{1, 2, 2}, {1, 3, 3}, {2, 3, 4}, {2, 4, 5}, {3, 4, 6}, {5, 1, 1}};
+        dijkstra(5, data, 1);
+    }
+}
